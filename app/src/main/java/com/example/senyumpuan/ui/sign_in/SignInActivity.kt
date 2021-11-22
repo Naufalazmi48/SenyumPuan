2b20088409ac38c6bc53452c959cbd9f8947018f
Examine88 <Naufalazmi020@gmail.com>
2021-11-22 23:02:55 +0800

    create splash screen and sign in logic UI

--- app/src/main/java/com/example/senyumpuan/ui/sign_in/SignInActivity.kt
+++ app/src/main/java/com/example/senyumpuan/ui/sign_in/SignInActivity.kt
@@ -1,12 +1,53 @@
 package com.example.senyumpuan.ui.sign_in
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.example.senyumpuan.BaseActivity
+import com.example.senyumpuan.MainActivity
 import com.example.senyumpuan.R
+import com.example.senyumpuan.databinding.ActivitySignInBinding
+import com.example.senyumpuan.ui.register.RegisterActivity
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity<ActivitySignInBinding>(), View.OnClickListener {
+    override fun getViewBinding(): ActivitySignInBinding =
+        ActivitySignInBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+
+        setupListener()
     }
+
+    private fun setupListener() {
+        binding.signIn.setOnClickListener(this)
+        binding.toRegister.setOnClickListener(this)
+    }
+
+    private fun validateForm(): Boolean =
+        binding.email.text.isNotEmpty() && binding.password.text.isNotEmpty()
+
+    override fun onClick(v: View) {
+        var intent: Intent? = null
+        when(v.id) {
+            R.id.sign_in -> {
+                if (validateForm()) {
+                    // TODO if login success
+                       intent = Intent(this@SignInActivity, MainActivity::class.java)
+                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                } else {
+                    // TODO invalid form action
+                }
+            }
+
+            R.id.to_register -> {
+                intent = Intent(this, RegisterActivity::class.java)
+            }
+
+        }
+
+        intent?.let { startActivity(it) }
+    }
+
 }
--- app/src/main/java/com/example/senyumpuan/ui/splashscreen/SplashActivity.kt
+++ app/src/main/java/com/example/senyumpuan/ui/splashscreen/SplashActivity.kt
@@ -1,12 +1,26 @@
 package com.example.senyumpuan.ui.splashscreen
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.example.senyumpuan.R
+import com.example.senyumpuan.ui.sign_in.SignInActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(
+                    Intent(this, SignInActivity::class.java)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                )
+            },
+            3000
+        )
     }
 }
