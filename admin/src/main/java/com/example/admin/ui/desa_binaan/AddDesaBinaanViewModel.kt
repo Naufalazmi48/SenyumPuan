df3ada2448910c51f80dcf1705036f49a6454f13
Examine88 <Naufalazmi020@gmail.com>
2021-12-10 14:04:17 +0800

    added forget file fromn local

---
+++ admin/src/main/java/com/example/admin/ui/desa_binaan/AddDesaBinaanViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.admin.ui.desa_binaan
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.data.Resource
+import com.example.core.domain.model.Desa
+import com.example.core.domain.usecase.maps.DesaBinaanUseCase
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class AddDesaBinaanViewModel(private val desaBinaanUseCase: DesaBinaanUseCase): ViewModel() {
+    val imagesUri = MutableLiveData<Uri>()
+
+    private val _desa = MutableLiveData<Resource<Boolean>>()
+    val desa: LiveData<Resource<Boolean>> = _desa
+
+    fun addLocationDesa(desa: Desa) {
+        viewModelScope.launch {
+            desaBinaanUseCase.addLocationDesaBinaan(desa).collect {
+                _desa.postValue(it)
+            }
+        }
+    }
+}
---
+++ admin/src/main/java/com/example/admin/ui/desa_binaan/ImageAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.admin.ui.desa_binaan
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.admin.R
+import com.example.admin.databinding.ItemImageBinding
+import kotlin.reflect.KFunction0
+
+class ImageAdapter(private val onAddAction: KFunction0<Unit>) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    private val listUri: ArrayList<Uri> = arrayListOf(Uri.EMPTY)
+
+    fun addImageUri(uri: Uri) {
+        listUri.add(uri)
+        notifyItemInserted(listUri.size-1)
+    }
+
+    fun getData(): List<String> = listUri.filter { it != Uri.EMPTY }. map { it.toString() }
+
+    inner class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = ItemImageBinding.bind(itemView)
+        fun bind(uri: Uri) {
+            with(binding) {
+                Glide.with(root)
+                    .load(uri)
+                    .into(image)
+
+                cancel.isVisible = true
+                imageAdd.isVisible = false
+
+                cancel.setOnClickListener {
+                    val position = listUri.indexOf(uri)
+                    listUri.remove(uri)
+                    notifyItemRemoved(position)
+                }
+            }
+        }
+
+        fun default() {
+            binding.root.setOnClickListener {
+                onAddAction.invoke()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder =
+        ImageViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false)
+        )
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        if (listUri[position] == Uri.EMPTY) {
+            holder.default()
+        } else {
+            holder.bind(listUri[position])
+        }
+    }
+
+    override fun getItemCount(): Int = listUri.size
+}
